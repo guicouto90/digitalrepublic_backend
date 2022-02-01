838532d629237f3598a86e8ddc7800568fa9514d
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2022-02-01 18:59:34 -0300

    Config for heroku deploy

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb').MongoClient;
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/DigitalRepublic';
+const MONGO_DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/DigitalRepublic';
 const DB_NAME = 'DigitalRepublic';
 
 module.exports = () => mongodb.connect(MONGO_DB_URL, {
